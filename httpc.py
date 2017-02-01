f87d599a85ff1e5ade71e16ad8222de0a2a8d71f
dsvoid <dsvoid@gmail.com>
2017-02-01 12:21:20 +0000

    verbosity now works for GET request

--- httpc.py
+++ httpc.py
@@ -3,28 +3,29 @@ import argparse
 from urllib.parse import urlparse
 
 
-def httpc_get(url):
+def httpc_get(url, verbose):
     # initialize the socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # urlparse is only used here to grab the hostname and request separately,
     # NOT to connect! (So, you know, please don't dock marks for it... thanks!)
     url = urlparse(url)
     s.connect((url.hostname, 80))
-    request = "GET " + url.path + "?" + url.query + "\r\n"
-    print(request)
+    if verbose:
+        request = "GET " + url.path + "?" + url.query + " HTTP/1.0\r\n\r\n"
+    else:
+        request = "GET " + url.path + "?" + url.query + " \r\n"
     s.sendall(request.encode("utf-8"))
-    response = s.recv(4096)
+    response = s.recv(4096) 
     print(response.decode("utf-8"))
     s.close
 
 
-def httpc_post(url, header, data):
+def httpc_post(url, header, data, verbose):
     # initialize the socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     url = urlparse(url)
     s.connect((url.hostname, 80))
     request = "POST " + url.path + "\r\n" + header + "\r\n" + data + "\r\n"
-    print(request)
     s.sendall(request.encode("utf-8"))
     response = s.recv(4096)
     print(response.decode("utf-8"))
@@ -41,16 +42,17 @@ dataVsFile = parser.add_mutually_exclusive_group()
 dataVsFile.add_argument('-d', '--data', help='data string for HTTP post', type=str)
 dataVsFile.add_argument('-f', '--file', help='file for HTTP post', type=str)
 verbose = parser.add_argument('-v', '--verbose', help='show verbose output',
-                              action='store_true')
+        action='store_true')
 
 args = parser.parse_args()
 if args.get is not None:
-    httpc_get(args.get)
+    httpc_get(args.get, args.verbose)
 elif args.post is not None:
     if args.data is not None:
-        httpc_post(args.post, args.header, args.data)
+        httpc_post(args.post, args.header, args.data,args.verbose)
     elif args.file is not None:
         f = open(args.file, 'r')
         data = file.read()
         f.close()
-        httpc_post(args.post, args.header, data)
+        httpc_post(args.post, args.header, data, args.verbose)
+

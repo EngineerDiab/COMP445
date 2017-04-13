7bda6cfb57e465090e31ed2877cc38379c7a8d09
dsvoid <dsvoid@gmail.com>
2017-04-13 10:29:30 +0100

    removed listen method

--- p2pchat.py
+++ p2pchat.py
@@ -5,10 +5,9 @@ host = ''
 port = 1337
 
 # set up socket
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind((host,port))
-listener.listen(1)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host,port))
 print("serving on port", port)
 while True:
     x = 1 # eheh ok cool

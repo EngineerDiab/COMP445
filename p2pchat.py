e04498861c823904c92d75a99c80df37779cfa67
dsvoid <dsvoid@gmail.com>
2017-04-13 10:26:34 +0100

    basic socket

--- p2pchat.py
+++ p2pchat.py
@@ -1,4 +1,14 @@
 import socket
 import os
 
+host = ''
+port = 1337
 
+# set up socket
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener.bind((host,port))
+listener.listen(1)
+print("serving on port", port)
+while True:
+    x = 1 # eheh ok cool

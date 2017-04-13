47565071687e1d6ea52ac13a1e6c7949fc3e551f
dsvoid <dsvoid@gmail.com>
2017-04-13 12:26:22 +0100

    chat with yourself

--- p2pchat.py
+++ p2pchat.py
@@ -1,13 +1,12 @@
 import socket
 import os
 import threading
+from datetime import datetime
 
 def sender(username, ip, port):
     # set up socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((ip, port))
-    print("serving on port", port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print("sending on port", port)
     while True:
         user_message = input()
         application_message = build_message(user_message, username)
@@ -16,28 +15,27 @@ def sender(username, ip, port):
 def build_message(user_message, user_name):
     return 'user: ' + user_name + '\nmessage: ' + user_message + '\n\n'
         
-def receiver(username, ip, socket):
+def receiver(username, ip, port):
     # set up socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((ip, port))
-    print("serving on port", port)
+    print("receiving on port", port)
     while True:
         appMsg = s.recv(4096)
         (username, userMsg) = parseMsg(appMsg)
-        print(username, userMsg)
+        print(datetime.now(),' [', username,']: ', userMsg, sep='')
 
 def parseMsg(appMsg):
+    appMsg = appMsg.decode('utf-8')
     appMsg = appMsg.split('\n')
-    username = appMsg[0]
-    userMsg = appMsg[1]
+    username = appMsg[0].split(': ')[1]
+    userMsg = appMsg[1].split(': ')
+    userMsg = ''.join(userMsg[1:])
     return (username, userMsg)
 
 p2 = os.fork()
 if p2 == 0:
-    sender('bobby', 'localhost', 1337)
-else:
     receiver('bobby', 'localhost', 1337)
+else:
+    sender('bobby', 'localhost', 1337)
 
-while True:
-    pass

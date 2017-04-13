c69826aefec5ea758284b32306f65d12446f505d
dsvoid <dsvoid@gmail.com>
2017-04-13 14:04:09 +0100

    /join

--- p2pchat.py
+++ p2pchat.py
@@ -6,37 +6,52 @@ from datetime import datetime
 def sender(username, ip, port):
     # set up socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-    print("sending on port", port)
-    while True:
-        user_message = input()
-        application_message = build_message(user_message, username)
-        s.sendto(application_message.encode('utf-8'), (ip, port))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    joinMsg = buildMsg(username, 'join', '')
+    s.sendto(joinMsg.encode('utf-8'), (ip, port))
+    running = True
+    while running:
+        userMsg = input()
+        command = 'talk'
+        if userMsg == '/leave':
+            command = 'leave'
+            running = False
+        appMsg = buildMsg(username, command, userMsg)
+        s.sendto(appMsg.encode('utf-8'), (ip, port))
 
-def build_message(user_message, user_name):
-    return 'user: ' + user_name + '\nmessage: ' + user_message + '\n\n'
+def buildMsg(username, command, userMsg):
+    return username + '\n' + command + '\n' + userMsg
         
 def receiver(username, ip, port):
     # set up socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((ip, port))
-    print("receiving on port", port)
+    print("logged on to port", port)
+    userlist = []
     while True:
         appMsg = s.recv(4096)
-        (username, userMsg) = parseMsg(appMsg)
-        print(datetime.now(),' [', username,']: ', userMsg, sep='')
+        (user, command, userMsg) = parseMsg(appMsg)
+        if command == 'talk':
+            print(datetime.now(),' [', user,']: ', userMsg, sep='')
+        if command == 'join':
+            print(datetime.now(), user, 'joined!')
+            userlist.add(user)
+        if command == 'leave':
+            print(datetime.now(), user, 'left...')
 
 def parseMsg(appMsg):
     appMsg = appMsg.decode('utf-8')
     appMsg = appMsg.split('\n')
-    username = appMsg[0].split(': ')[1]
-    userMsg = appMsg[1].split(': ')
-    userMsg = ''.join(userMsg[1:])
-    return (username, userMsg)
+    username = appMsg[0]
+    command = appMsg[1]
+    userMsg = appMsg[2]
+    return (username, command, userMsg)
 
+
+name = input('enter your name: ')
 p2 = os.fork()
 if p2 == 0:
-    receiver('bobby', '255.255.255.255', 1337)
+    receiver(name, '255.255.255.255', 1337)
 else:
-    sender('bobby', '255.255.255.255', 1337)
+    sender(name, '255.255.255.255', 1337)
 

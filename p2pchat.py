ad7e6ece44fe6495ad895e62770ead00ea1c6882
dsvoid <dsvoid@gmail.com>
2017-04-13 10:47:45 +0100

    receiver and parseMsg

--- p2pchat.py
+++ p2pchat.py
@@ -1,13 +1,20 @@
 import socket
 import os
+import threading
 
-host = ''
-port = 1337
+def receiver(username, ip, port):
+    # set up socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((ip,port))
+    print("serving on port", port)
+    while True:
+        appMsg = s.recv(1024)
+        (username, userMsg) = parseMsg(appMsg)
+        print(username, userMsg)
 
-# set up socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host,port))
-print("serving on port", port)
-while True:
-    x = 1 # eheh ok cool
+def parseMsg(appMsg):
+    appMsg = appMsg.split('\n')
+    username = appMsg[0]
+    userMsg = appMsg[1]
+    return (username, userMsg)

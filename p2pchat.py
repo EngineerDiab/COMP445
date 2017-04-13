598e0bfca0acc07eb193c32f86bf842b3513439e
Mathieu Diab <diabmathieu@gmail.com>
2017-04-13 09:43:54 -0400

    sender and buildmsg

--- p2pchat.py
+++ p2pchat.py
@@ -10,4 +10,21 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host,port))
 print("serving on port", port)
 while True:
-    x = 1 # eheh ok cool
+    x = 1
+
+
+def buildMessage(userMessage, userName):
+    return 'user: ' + userName + '\nmessage: ' + userMessage + '\n\n'
+
+
+def sender(userName, IPAddress, port):
+    # set up socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    print("serving on port", port)
+    while True:
+        userMessage = input()
+        applicationMessage = buildMessage(userMessage, userName)
+        s.sendto(applicationMessage, IPAddress, port)
+
